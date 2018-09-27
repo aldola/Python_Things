e7685874b32ee0e503d1f6355cf49936b930e73a
Albert <albertdom.ba2@gmail.com>
2018-09-27 18:15:36 +0200

    Added ForestSimulator Class
    Works?... I think not

--- fire_module.py
+++ fire_module.py
@@ -1,10 +1,13 @@
+from graphics import *
 import  random
 
 class Cell:
 
     state = 'E'
     PBURN   = 0.00006
+    #6/100000
     PGROWTH = 0.01
+    #1/100
 
     def isBurning(self):
         return self.state == 'B'
@@ -16,12 +19,19 @@ class Cell:
         return self.state == 'T'
     
     def step(self,  hasBurningNeighbour):
-        if self.hasTree() and hasBurningNeighbour:
-            self.state = 'B'
+        if self.hasTree():
+            if hasBurningNeighbour:
+                self.state = 'B'
+            elif random.randint(1,1000) <= 6:
+                self.state = 'B'
+        elif self.isEmpty():
+            if random.randint(1,10) == 1:
+                self.state = 'T'
         elif self.isBurning():
             self.state = 'E'
-        else:
-            self.state = 'T'
+
+    def getState(self):
+        return self.state
 
 class Coordinates:
 
@@ -32,10 +42,10 @@ class Coordinates:
         self.x = x
         self.y = y
 
-    def getX():
+    def getX(self):
         return self.x
 
-     def getY():
+    def getY(self):
         return self.y 
 
 class Forest:
@@ -54,38 +64,87 @@ class Forest:
             self.area.append(column)
 
     def step(self):
-        w = random.randint(0,width-1)
-        h = random.randint(0,height-1)
+        w = random.randint(0,self.width-1)
+        h = random.randint(0,self.height-1)
         self.cellAt(w,h).step(self.hasBurningNeighbour(w,h))
         return Coordinates(w,h)
 
     def cellAt(self,x,y):
         if x != self.width and y != self.height:
-            return self.area[x][y].Cell()
+            return self.area[x][y]
         else:
             return None
 
-    def hasBurningNeighbour(x,y):
-        #Left
-        if w != 0:
-            if self.area[w-1][h].isBurning():
-                burningNeighbour = True
-        #Right
-        if w != width-1 and not burningNeighbour:
-            if self.area[w+1][h].isBurning():
-                burningNeighbour = True 
-        #Up
-        if h != 0 and not burningNeighbour:
-            if self.area[w][h-1].isBurning():
-                burningNeighbour = True
-        #Down
-        if h != height-1 and not burningNeighbour:
-            if self.area[w][h+1].isBurning():
-                burningNeighbour = True
-        self.area[w][h].step(burningNeighbour)
+    def hasBurningNeighbour(self,x,y):
+
+        for i in range(x-1,x+1):
+            for j in range(y-1,y+1):
+                if self.cellAt(i,j) is not None and (i != x and j != y):
+                    if self.cellAt(i,j).isBurning():
+                        return True
+        return False
+
+    def countTrees():
+        t = 0
+        for i in range(0,self.width):
+            for j in range(0,self.height):    
+                if self.cellAt(i,j).hasTree():
+                    t += 1
+        return t     
+    
+    def countFires():
+        f = 0
+        for i in range(0,self.width):
+            for j in range(0,self.height):    
+                if self.cellAt(i,j).isBurning():
+                    f += 1
+        return f
+
+class ForestSimulator: 
+    WIDTH  = 20
+    HEIGHT = 20
+    CELL_SIZE = 4
+    APLLICATION_WIDTH  = WIDTH  * CELL_SIZE
+    APLLICATION_HEIGHT = HEIGHT * CELL_SIZE
+    EMPTY   = "black"
+    TREE    = "green"
+    BURNING = "red"
+
+    def run(self):
+        window = []
+        win = GraphWin('Forest', self.APLLICATION_WIDTH, self.APLLICATION_HEIGHT)
+        for x in range(self.WIDTH):
+            column = []
+            for y in range(self.HEIGHT):
+                b = Rectangle(Point(x*self.CELL_SIZE, y*self.CELL_SIZE),Point(4, 4))
+                b.setFill(self.EMPTY)
+                b.draw(win)
+                column.append(b)
+            window.append(column)
+        while True:
+            forest = Forest(self.WIDTH,self.HEIGHT)
+            cordinates = forest.step()
+            state = forest.cellAt(cordinates.getX(),cordinates.getY()).getState()
+            print (state)
+            print (window[cordinates.getX()][cordinates.getY()])
+            if state == 'E':
+                window[cordinates.getX()][cordinates.getY()].setFill(self.EMPTY)
+            elif state == 'T':
+                window[cordinates.getX()][cordinates.getY()].setFill(self.TREE)
+            elif state == 'B':        
+                window[cordinates.getX()][cordinates.getY()].setFill(self.BURNING)
+            time.sleep(.01)
+            #win.getMouse()
 
 if __name__ == "__main__":
-
-    c = random.randint(0,5-1)
-    print (c)
-
+    a = ForestSimulator()
+    a.run()
+    """     win = GraphWin('Forest', 100, 100)
+    b = Rectangle(Point(4, 4), Point(4, 4))
+    c = Rectangle(Point(20, 20), Point(30, 30))
+    b.setFill("red")
+    b.draw(win)  
+    c.setFill("brown")
+    c.draw(win)   
+    win.getMouse() 
+    win.close() """
